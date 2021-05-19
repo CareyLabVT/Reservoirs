4a608582972f33ba9634ce540ebe6ecb08cd3a06
abbylewis <aslewis@vt.edu>
2021-05-19 15:11:06 -0400

    Setting up platform/ctd comparison at BVR

--- Data/DataNotYetUploadedToEDI/Raw_CTD/CTD_code/CTD_matlab_functions.R
+++ Data/DataNotYetUploadedToEDI/Raw_CTD/CTD_code/CTD_matlab_functions.R
@@ -1,12 +1,11 @@
-fcr_data_wrangling <- function(){
+fcr_data_wrangling <- function(site){
   ### Transforming the outputted csv files to a MATLAB format
   ### Reason I am plotting in R and Not matlab is because MATLAB
   ### makes for better Heatmap plots
   
   # This reads all the files into the R environment
   files = list.files(path = "../csv_outputs/")
-  #files <- files[files != "090419_fcr50.csv"]
-  files = files[grepl("fcr50",files)]
+  files = files[grepl(site,files)]
   
   #This reads the first file in
   ctd = read.csv(paste0("../csv_outputs/",files[1])) 
@@ -20,7 +19,7 @@ fcr_data_wrangling <- function(){
     }
   }
   
-  write_csv(ctd, "../CTD_season_csvs/CTD_notmatlab_ready_2021_fcr50.csv")
+  write_csv(ctd, paste0("../CTD_season_csvs/CTD_notmatlab_ready_2021_", site, ".csv"))
   
   # Convert dates to MatLab dates
   POSIXt2matlabUTC = function(x) {
@@ -45,7 +44,7 @@ fcr_data_wrangling <- function(){
   ctd_matlab$Date <- POSIXt2matlabUTC(ctd_matlab$Date)
   
   
-  write_csv(ctd_matlab, "../CTD_season_csvs/CTD_matlab_ready_2021_fcr50.csv", col_names = F)
+  write_csv(ctd_matlab, paste0("../CTD_season_csvs/CTD_matlab_ready_2021_", site, ".csv"), col_names = F)
   
   
   # filter out depths in the CTD cast that are closest to the specified values for FLARE - AED 
@@ -68,116 +67,177 @@ fcr_data_wrangling <- function(){
   ctd_new$Depth_m <- round(as.numeric(ctd_new$Depth_m), digits = 0.5)
   ctd_new$Date <- ymd_hms(ctd_new$Date)
   
-  write_csv(ctd_new, "../CTD_season_csvs/ctd_short_for_GLM_AED_2021.csv")
+  write_csv(ctd_new, paste0("../CTD_season_csvs/ctd_short_for_GLM_AED",site,"_2021.csv"))
   
-  
-  # # Pulling just temp, depth and date and going from long to wide. 
-  # temp_RLA <- ctd_new %>%
-  #   select(Date,Depth_m,Temp_C)%>%
-  #   spread(Depth_m,Temp_C)
-  # 
-  # # renaming the column names to include wtr_ 
-  # # Otherwise, rLakeAnaylzer will not run!
-  # colnames(temp_RLA)[-1] = paste0('wtr_',colnames(temp_RLA)[-1])
-  # 
-  # # rename the first column to "datetime"
-  # names(temp_RLA)[1] <- "datetime"
-  # 
-  # # Calculate thermocline depth
-  # FCR_thermo_18 <- ts.thermo.depth(temp_RLA)
-  # 
-  # #rename the datetime name back to Date
-  # names(FCR_thermo_18)[1] <- "Date"
   print("Success! Matlab files and glm aed files have been created. Double check they look ok!")
   return(ctd_new)
 }
 
 
-
-
-ctd_vs_catwalk <- function(on,off,startDate = "2021-01-01 12:00:00"){
+ctd_vs_catwalk <- function(on,off,site,startDate = "2021-01-01 12:00:00"){
   # Compare the CTD and catwalk data. 
   # RPM 18June2019
   #Substantial edits by ASL 23 Jun 20. Selecting the depths closest to the actual catwalk depth
   
   pacman::p_load(tidyverse, rLakeAnalyzer)
-  cat <- read_csv(file = getURL("https://raw.githubusercontent.com/FLARE-forecast/FCRE-data/fcre-catwalk-data/Catwalk.csv"),skip = 1)
-  ctd_new = read_csv("../CTD_season_csvs/CTD_notmatlab_ready_2021_fcr50.csv")
+  if(site == "fcr50"){
+    
+    #### FCR ####
+    cat <- read_csv(file = getURL("https://raw.githubusercontent.com/FLARE-forecast/FCRE-data/fcre-catwalk-data/Catwalk.csv"),skip = 1)
+    ctd_new = read_csv("../CTD_season_csvs/CTD_notmatlab_ready_2021_fcr50.csv")
   
-  cat_sum_19 <- cat %>% filter(TIMESTAMP >= startDate) %>%
-    select(TIMESTAMP, doobs_1, doobs_5, doobs_9,EXO_depth) %>%
-    filter(doobs_1 != "NAN") %>%
-    filter(doobs_5 != "NAN") %>%
-    filter(doobs_9 != "NAN") %>%
-    filter(TIMESTAMP != "NAN") %>%
-    filter(TIMESTAMP != "YYYY_MM_DD_HH_MM_SS")
+    cat_sum_19 <- cat %>% filter(TIMESTAMP >= startDate) %>%
+      select(TIMESTAMP, doobs_1, doobs_5, doobs_9,EXO_depth) %>%
+      filter(doobs_1 != "NAN") %>%
+      filter(doobs_5 != "NAN") %>%
+      filter(doobs_9 != "NAN") %>%
+      filter(TIMESTAMP != "NAN") %>%
+      filter(TIMESTAMP != "YYYY_MM_DD_HH_MM_SS")
+    
+    dates = unique(ctd_new$Date)
+    ctd_new$EXO_depth=NA
+    for(date in dates){
+      ctd_new$EXO_depth[ctd_new$Date==date] = cat_sum_19$EXO_depth[which.min(abs(as.numeric(as.POSIXct(cat_sum_19$TIMESTAMP))-date))]
+    }
+    class(ctd_new$EXO_depth)="numeric"
+    
+    ctd_1.0 <- ctd_new %>% 
+      group_by(Date)%>%
+      filter(abs(Depth_m-EXO_depth)==min(abs(Depth_m-EXO_depth)))
+    
+    ctd_5.0 <- ctd_new %>% 
+      group_by(Date)%>%
+      filter(abs(Depth_m-(EXO_depth +5 -1.6))==min(abs(Depth_m-(EXO_depth +5 -1.6))))
+    
+    ctd_9.0 <- ctd_new %>% 
+      group_by(Date)%>%
+      filter(abs(Depth_m-(EXO_depth +9 -1.6))==min(abs(Depth_m-(EXO_depth +9 -1.6))))
+    
+    jpeg("../CTD_catwalk_figures/SEASONAL_CATWALK_CTD_COMPARE_DO_2021.jpg", width=14, height=8, units = "in",res = 300)
+    plot(as_datetime(cat_sum_19$TIMESTAMP), cat_sum_19$doobs_1, type = "l", ylim = c(0,14), xlab = "", ylab = "DO (mg/L)")
+    lines(as_datetime(cat_sum_19$TIMESTAMP), cat_sum_19$doobs_5, type = "l", ylim = c(0,14), col = "blue")
+    lines(as_datetime(cat_sum_19$TIMESTAMP), cat_sum_19$doobs_9, type = "l", ylim = c(0,14), col = "magenta")
+    points(ctd_1.0$Date, ctd_1.0$DO_mgL, type = "p", pch = 21, col = "black", bg = "black", cex = 2, lwd = 2)
+    points(ctd_5.0$Date, ctd_5.0$DO_mgL, type = "p", pch = 21, col = "black", bg = "blue", cex = 2, lwd = 2)
+    points(ctd_9.0$Date, ctd_9.0$DO_mgL, type = "p", pch = 21, col = "black", bg = "magenta", cex = 2, lwd = 2)
+    abline(v=on, lwd = 1.5)
+    abline(v=off,lty=2, lwd = 1.5)
+    legend("topright", legend=c("1.6 m", "5 m", "9 m"), pch = 21, pt.bg = c("black","blue","magenta"))
+    dev.off()
+    
+    ### THERE IS NO dotemp_1 ???
+    cat_sum_19_temp <- cat %>% filter(TIMESTAMP >= startDate) %>%
+      select(TIMESTAMP, wtr_1, dotemp_5, dotemp_9) %>%
+      filter(wtr_1 != "NAN") %>%
+      filter(dotemp_5 != "NAN") %>%
+      filter(dotemp_9 != "NAN") %>%
+      filter(TIMESTAMP != "NAN") %>%
+      filter(TIMESTAMP != "YYYY_MM_DD_HH_MM_SS")
+    
+    
+    jpeg("../CTD_catwalk_figures/SEASONAL_CATWALK_CTD_COMPARE_TEMP_2021.jpg", width=14, height=8, units = "in",res = 300)
+    plot(as_datetime(cat_sum_19_temp$TIMESTAMP), cat_sum_19_temp$wtr_1, type = "l", ylim = c(4,33), xlab = "", ylab = "Temp (C)")
+    lines(as_datetime(cat_sum_19_temp$TIMESTAMP), cat_sum_19_temp$dotemp_5, type = "l", ylim = c(4,33), col = "blue")
+    lines(as_datetime(cat_sum_19_temp$TIMESTAMP), cat_sum_19_temp$dotemp_9, type = "l", ylim = c(4,33), col = "magenta")
+    points(ctd_1.0$Date, ctd_1.0$Temp_C, type = "p", pch = 21, col = "black", bg = "black", cex = 2, lwd = 2)
+    points(ctd_5.0$Date, ctd_5.0$Temp_C, type = "p", pch = 21, col = "black", bg = "blue", cex = 2, lwd = 2)
+    points(ctd_9.0$Date, ctd_9.0$Temp_C, type = "p", pch = 21, col = "black", bg = "magenta", cex = 2, lwd = 2)
+    abline(v=on, lwd = 1.5)
+    abline(v=off,lty=2, lwd = 1.5)
+    legend("topright", legend=c("1.6 m", "5 m", "9 m"), pch = 21, pt.bg = c("black","blue","magenta"))
+    dev.off()
+    
+    cat_sum_19_chla <- cat %>% filter(TIMESTAMP >= startDate) %>%
+      select(TIMESTAMP, Chla_1 ) %>%
+      filter(Chla_1 != "NAN") %>%
+      filter(TIMESTAMP != "NAN") %>%
+      filter(TIMESTAMP != "YYYY_MM_DD_HH_MM_SS")
+    
+    jpeg("../CTD_catwalk_figures/SEASONAL_CATWALK_CTD_COMPARE_CHLA_2021.jpg", width=14, height=8, units = "in",res = 300)
+    plot(as_datetime(cat_sum_19_chla$TIMESTAMP), cat_sum_19_chla$Chla_1, type = "l", ylim = c(0,50), xlab = "", ylab = "chla (ug/L)")
+    points(ctd_1.0$Date, ctd_1.0$Chla_ugL, type = "p", pch = 21, col = "black", bg = "green", cex = 2, lwd = 2)
+    abline(v=on, lwd = 1.5)
+    abline(v=off,lty=2, lwd = 1.5)
+    legend("topright", legend=c("CTD", "catwalk"), pch = c(21,NA), lty = c(NA,1), pt.bg = c("green",NA))
+    dev.off()
   
-  dates = unique(ctd_new$Date)
-  ctd_new$EXO_depth=NA
-  for(date in dates){
-    ctd_new$EXO_depth[ctd_new$Date==date] = cat_sum_19$EXO_depth[which.min(abs(as.numeric(as.POSIXct(cat_sum_19$TIMESTAMP))-date))]
+  }else if(site == "bvr50"){
+    
+    
+    #### BVR #####
+    
+    cat <- read_csv(file = getURLContent("https://raw.githubusercontent.com/FLARE-forecast/BVRE-data/bvre-platform-data/BVRplatform.csv"),skip = 1)
+    ctd_new = read_csv("../CTD_season_csvs/CTD_notmatlab_ready_2021_bvr50.csv")
+    
+    cat_sum_19 <- cat %>% filter(TIMESTAMP >= startDate) %>%
+      select(TIMESTAMP, doobs_1, doobs_6, doobs_13,EXO_depth) %>%
+      filter(doobs_1 != "NAN") %>%
+      filter(doobs_6 != "NAN") %>%
+      filter(doobs_13 != "NAN") %>%
+      filter(TIMESTAMP != "NAN") %>%
+      filter(TIMESTAMP != "YYYY_MM_DD_HH_MM_SS")
+    
+    
+    ctd_1.0 <- ctd_new %>% 
+      group_by(Date)%>%
+      filter(abs(Depth_m-EXO_depth)==min(abs(Depth_m-EXO_depth)))
+    
+    ctd_4.0 <- ctd_new %>% 
+      group_by(Date)%>%
+      filter(abs(Depth_m-4)==min(abs(Depth_m-4)))
+    
+    ctd_11.0 <- ctd_new %>% 
+      group_by(Date)%>%
+      filter(abs(Depth_m-11)==min(abs(Depth_m-11)))
+    
+    jpeg("../CTD_catwalk_figures/SEASONAL_CATWALK_CTD_COMPARE_DO_2021.jpg", width=14, height=8, units = "in",res = 300)
+    plot(as_datetime(cat_sum_19$TIMESTAMP), cat_sum_19$doobs_1, type = "l", ylim = c(0,14), xlab = "", ylab = "DO (mg/L)")
+    lines(as_datetime(cat_sum_19$TIMESTAMP), cat_sum_19$doobs_6, type = "l", ylim = c(0,14), col = "blue")
+    lines(as_datetime(cat_sum_19$TIMESTAMP), cat_sum_19$doobs_13, type = "l", ylim = c(0,14), col = "magenta")
+    points(ctd_1.0$Date, ctd_1.0$DO_mgL, type = "p", pch = 21, col = "black", bg = "black", cex = 2, lwd = 2)
+    points(ctd_4.0$Date, ctd_4.0$DO_mgL, type = "p", pch = 21, col = "black", bg = "blue", cex = 2, lwd = 2)
+    points(ctd_11.0$Date, ctd_11.0$DO_mgL, type = "p", pch = 21, col = "black", bg = "magenta", cex = 2, lwd = 2)
+    abline(v=on, lwd = 1.5)
+    abline(v=off,lty=2, lwd = 1.5)
+    legend("topright", legend=c("1.5 m", "4 m", "11 m"), pch = 21, pt.bg = c("black","blue","magenta"))
+    dev.off()
+    
+    ### THERE IS NO dotemp_1 ???
+    cat_sum_19_temp <- cat %>% filter(TIMESTAMP >= startDate) %>%
+      select(TIMESTAMP, wtr_1, dotemp_6, dotemp_13) %>%
+      filter(wtr_1 != "NAN") %>%
+      filter(dotemp_5 != "NAN") %>%
+      filter(dotemp_9 != "NAN") %>%
+      filter(TIMESTAMP != "NAN") %>%
+      filter(TIMESTAMP != "YYYY_MM_DD_HH_MM_SS")
+    
+    
+    jpeg("../CTD_catwalk_figures/SEASONAL_CATWALK_CTD_COMPARE_TEMP_2021.jpg", width=14, height=8, units = "in",res = 300)
+    plot(as_datetime(cat_sum_19_temp$TIMESTAMP), cat_sum_19_temp$wtr_1, type = "l", ylim = c(4,33), xlab = "", ylab = "Temp (C)")
+    lines(as_datetime(cat_sum_19_temp$TIMESTAMP), cat_sum_19_temp$dotemp_6, type = "l", ylim = c(4,33), col = "blue")
+    lines(as_datetime(cat_sum_19_temp$TIMESTAMP), cat_sum_19_temp$dotemp_13, type = "l", ylim = c(4,33), col = "magenta")
+    points(ctd_1.0$Date, ctd_1.0$Temp_C, type = "p", pch = 21, col = "black", bg = "black", cex = 2, lwd = 2)
+    points(ctd_4.0$Date, ctd_4.0$Temp_C, type = "p", pch = 21, col = "black", bg = "blue", cex = 2, lwd = 2)
+    points(ctd_11.0$Date, ctd_11.0$Temp_C, type = "p", pch = 21, col = "black", bg = "magenta", cex = 2, lwd = 2)
+    abline(v=on, lwd = 1.5)
+    abline(v=off,lty=2, lwd = 1.5)
+    legend("topright", legend=c("1.5 m", "4 m", "11 m"), pch = 21, pt.bg = c("black","blue","magenta"))
+    dev.off()
+    
+    cat_sum_19_chla <- cat %>% filter(TIMESTAMP >= startDate) %>%
+      select(TIMESTAMP, Chla_1 ) %>%
+      filter(Chla_1 != "NAN") %>%
+      filter(TIMESTAMP != "NAN") %>%
+      filter(TIMESTAMP != "YYYY_MM_DD_HH_MM_SS")
+    
+    jpeg("../CTD_catwalk_figures/SEASONAL_CATWALK_CTD_COMPARE_CHLA_2021.jpg", width=14, height=8, units = "in",res = 300)
+    plot(as_datetime(cat_sum_19_chla$TIMESTAMP), cat_sum_19_chla$Chla_1, type = "l", ylim = c(0,50), xlab = "", ylab = "chla (ug/L)")
+    points(ctd_1.0$Date, ctd_1.0$Chla_ugL, type = "p", pch = 21, col = "black", bg = "green", cex = 2, lwd = 2)
+    abline(v=on, lwd = 1.5)
+    abline(v=off,lty=2, lwd = 1.5)
+    legend("topright", legend=c("CTD", "catwalk"), pch = c(21,NA), lty = c(NA,1), pt.bg = c("green",NA))
+    dev.off()
   }
-  class(ctd_new$EXO_depth)="numeric"
-  
-  ctd_1.0 <- ctd_new %>% 
-    group_by(Date)%>%
-    filter(abs(Depth_m-EXO_depth)==min(abs(Depth_m-EXO_depth)))
-  
-  ctd_5.0 <- ctd_new %>% 
-    group_by(Date)%>%
-    filter(abs(Depth_m-(EXO_depth +5 -1.6))==min(abs(Depth_m-(EXO_depth +5 -1.6))))
-  
-  ctd_9.0 <- ctd_new %>% 
-    group_by(Date)%>%
-    filter(abs(Depth_m-(EXO_depth +9 -1.6))==min(abs(Depth_m-(EXO_depth +9 -1.6))))
-  
-  jpeg("../CTD_catwalk_figures/SEASONAL_CATWALK_CTD_COMPARE_DO_2021.jpg", width=14, height=8, units = "in",res = 300)
-  plot(as_datetime(cat_sum_19$TIMESTAMP), cat_sum_19$doobs_1, type = "l", ylim = c(0,14), xlab = "", ylab = "DO (mg/L)")
-  lines(as_datetime(cat_sum_19$TIMESTAMP), cat_sum_19$doobs_5, type = "l", ylim = c(0,14), col = "blue")
-  lines(as_datetime(cat_sum_19$TIMESTAMP), cat_sum_19$doobs_9, type = "l", ylim = c(0,14), col = "magenta")
-  points(ctd_1.0$Date, ctd_1.0$DO_mgL, type = "p", pch = 21, col = "black", bg = "black", cex = 2, lwd = 2)
-  points(ctd_5.0$Date, ctd_5.0$DO_mgL, type = "p", pch = 21, col = "black", bg = "blue", cex = 2, lwd = 2)
-  points(ctd_9.0$Date, ctd_9.0$DO_mgL, type = "p", pch = 21, col = "black", bg = "magenta", cex = 2, lwd = 2)
-  abline(v=on, lwd = 1.5)
-  abline(v=off,lty=2, lwd = 1.5)
-  legend("topright", legend=c("1.6 m", "5 m", "9 m"), pch = 21, pt.bg = c("black","blue","magenta"))
-  dev.off()
-  
-  ### THERE IS NO dotemp_1 ???
-  cat_sum_19_temp <- cat %>% filter(TIMESTAMP >= startDate) %>%
-    select(TIMESTAMP, wtr_1, dotemp_5, dotemp_9) %>%
-    filter(wtr_1 != "NAN") %>%
-    filter(dotemp_5 != "NAN") %>%
-    filter(dotemp_9 != "NAN") %>%
-    filter(TIMESTAMP != "NAN") %>%
-    filter(TIMESTAMP != "YYYY_MM_DD_HH_MM_SS")
-  
-  
-  jpeg("../CTD_catwalk_figures/SEASONAL_CATWALK_CTD_COMPARE_TEMP_2021.jpg", width=14, height=8, units = "in",res = 300)
-  plot(as_datetime(cat_sum_19_temp$TIMESTAMP), cat_sum_19_temp$wtr_1, type = "l", ylim = c(4,33), xlab = "", ylab = "Temp (C)")
-  lines(as_datetime(cat_sum_19_temp$TIMESTAMP), cat_sum_19_temp$dotemp_5, type = "l", ylim = c(4,33), col = "blue")
-  lines(as_datetime(cat_sum_19_temp$TIMESTAMP), cat_sum_19_temp$dotemp_9, type = "l", ylim = c(4,33), col = "magenta")
-  points(ctd_1.0$Date, ctd_1.0$Temp_C, type = "p", pch = 21, col = "black", bg = "black", cex = 2, lwd = 2)
-  points(ctd_5.0$Date, ctd_5.0$Temp_C, type = "p", pch = 21, col = "black", bg = "blue", cex = 2, lwd = 2)
-  points(ctd_9.0$Date, ctd_9.0$Temp_C, type = "p", pch = 21, col = "black", bg = "magenta", cex = 2, lwd = 2)
-  abline(v=on, lwd = 1.5)
-  abline(v=off,lty=2, lwd = 1.5)
-  legend("topright", legend=c("1.6 m", "5 m", "9 m"), pch = 21, pt.bg = c("black","blue","magenta"))
-  dev.off()
-  
-  cat_sum_19_chla <- cat %>% filter(TIMESTAMP >= startDate) %>%
-    select(TIMESTAMP, Chla_1 ) %>%
-    filter(Chla_1 != "NAN") %>%
-    filter(TIMESTAMP != "NAN") %>%
-    filter(TIMESTAMP != "YYYY_MM_DD_HH_MM_SS")
-  
-  jpeg("../CTD_catwalk_figures/SEASONAL_CATWALK_CTD_COMPARE_CHLA_2021.jpg", width=14, height=8, units = "in",res = 300)
-  plot(as_datetime(cat_sum_19_chla$TIMESTAMP), cat_sum_19_chla$Chla_1, type = "l", ylim = c(0,50), xlab = "", ylab = "chla (ug/L)")
-  points(ctd_1.0$Date, ctd_1.0$Chla_ugL, type = "p", pch = 21, col = "black", bg = "green", cex = 2, lwd = 2)
-  abline(v=on, lwd = 1.5)
-  abline(v=off,lty=2, lwd = 1.5)
-  legend("topright", legend=c("CTD", "catwalk"), pch = c(21,NA), lty = c(NA,1), pt.bg = c("green",NA))
-  dev.off()
   
   print("Success! Catwalk comparison figures have been created. Double check they look ok!")
 }

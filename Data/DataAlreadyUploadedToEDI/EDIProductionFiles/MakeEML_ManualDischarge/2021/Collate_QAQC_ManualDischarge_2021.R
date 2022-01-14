df4bd3eff6c8118de54e90b9605a541e325764a9
wwoel <wwoelmer@vt.edu>
2022-01-14 10:36:17 -0500

    add bucket data and format

--- Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEML_ManualDischarge/2021/Collate_QAQC_ManualDischarge_2021.R
+++ Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEML_ManualDischarge/2021/Collate_QAQC_ManualDischarge_2021.R
@@ -3,19 +3,18 @@ library(tidyverse)
 library(ggplot2)
 
 # read in new data, format, and plot for QAQC
-newdata <- read.csv('./Data/DataNotYetUploadedToEDI/Raw_inflow/Wetland_Discharge_Data_2020_2021.csv')
+newdata <- read.csv('./Data/DataNotYetUploadedToEDI/Raw_inflow/Manual_Discharge/Calculated_Discharge_Flowmate_Data_2020_2021.csv')
 newdata$Date <- as.Date(newdata$Date)
 newdata$Reservoir <- NA
-newdata$Method <- NA
+newdata$Method <- 'F'
 newdata$Flag_Flow <- 0
 
 for(i in 1:nrow(newdata)){
   if(newdata$Site[i]=='F200'){
-    newdata$Reservoir <- 'FCR'
+    newdata$Reservoir[i] <- 'FCR'
     newdata$Site[i] <- 200
-  }
-  if(newdata$FlowmeterSensorID[i]=='flowmate new'){
-    newdata$Method[i] <- F
+  }else{
+    newdata$Reservoir[i] <- 'CCR'
   }
 }
 
@@ -23,10 +22,32 @@ newdata <- newdata %>% select(Reservoir, Site, Date, Discharge_m3_s, Method, Fla
 colnames(newdata) <- c('Reservoir', 'Site', 'Date', 'Flow_cms', 'Method', 'Flag_Flow')
 
 ggplot(data = newdata, aes(x = Date, y = Flow_cms)) + 
-  geom_line() 
+  geom_line() +
+  facet_wrap(~Reservoir)
+
+# read in bucket and float velocity data
+bucket <- read.csv('./Data/DataNotYetUploadedToEDI/Raw_inflow/Manual_Discharge/CCR_bucket_data_2021.csv')
+bucket <- bucket %>% select(Reservoir, Site, DateTime, Discharge_cms, Method)
+bucket$Flag_Flow <- 0
+colnames(bucket) <- c('Reservoir', 'Site', 'Date', 'Flow_cms', 'Method', 'Flag_Flow')
+bucket$Date <- as.Date(bucket$Date)
 
 # combine with previous published data
 olddata <- read.csv('./Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEML_ManualDischarge/2019/2019_Continuum_Discharge.csv')
+olddata$Date <- as.Date(olddata$Date)
 
 edi <- rbind(olddata, newdata)
+edi <- rbind(edi, bucket)
+edi$Date <- as.Date(edi$Date)
+
+# sort by date
+edi <- edi[order(as.Date(edi$Date)),]
+
+# plot data
+ggplot(data = edi, aes(x = Date, y = Flow_cms)) + 
+  geom_point(aes(color = Site)) +
+  facet_wrap(~Reservoir, scales = 'free_y')
+
+edi$Flag_Flow <- as.character(edi$Flag_Flow)
+
 write.csv(edi, './Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEML_ManualDischarge/2021/ManualDischarge_2019_2021.csv', row.names = FALSE)

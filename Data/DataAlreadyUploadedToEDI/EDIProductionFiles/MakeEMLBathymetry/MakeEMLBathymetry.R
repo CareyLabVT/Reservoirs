009945d079804abacf040ba9b309a927eec0494f
abbylewis <aslewis@vt.edu>
2022-11-08 07:39:03 -0500

    Bathymetry revisions with new watershed/TIN files

--- Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEMLBathymetry/MakeEMLBathymetry.R
+++ Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEMLBathymetry/MakeEMLBathymetry.R
@@ -2,12 +2,15 @@ library(EMLassemblyline)
 
 make_eml(
   path = ".",
-  dataset.title = "Bathymetry for Falling Creek Reservoir and Beaverdam Reservoir, Virginia, USA",
+  dataset.title = "Bathymetry and watershed area for Falling Creek Reservoir, Beaverdam Reservoir, and Carvins Cove Reservoir",
   temporal.coverage = c("2012-07-12", "2014-07-22"),
   maintenance.description = 'completed',
   data.table = c("Bathymetry_comb.csv"),
   data.table.name = c("Bathymetric summary statistics"),
   data.table.description = c("Data table including bathymetric summary statistics for both reservoirs"),
+  other.entity = c("Bathymetry.zip","Watersheds.zip"),
+  other.entity.name = c("Bathymetry spatial data","Watershed spatial data"),
+  other.entity.description = c("Spatial data for bathymetry from all reservoirs","Spatial data for watershed area from all reservoirs"),
   user.id = 'ccarey',
   user.domain = 'EDI',
-  package.id = 'edi.939.2')
+  package.id = 'edi.971.1')

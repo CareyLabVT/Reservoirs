f4d4fe0137793c4e404667f301463cdc83176bd2
addelany <addelany@gmail.com>
2023-09-08 11:07:07 -0500

    make code into function

--- Scripts/qaqc_action_scripts/secchi_qaqc.R
+++ Scripts/qaqc_action_scripts/secchi_qaqc.R
@@ -3,6 +3,8 @@
 library(tidyverse)
 library(gsheet)
 
+secchi_qaqc <- function(){
+  
 gsheet_url <- 'https://docs.google.com/spreadsheets/d/1fvM0fDRliuthicQWZT7c9RYErikI5DwrzbOC7TCoMGI/edit#gid=1172894977'
 secchi_df <- gsheet::gsheet2tbl(gsheet_url)
 
@@ -12,3 +14,7 @@ secchi_reformat <- secchi_df |>
   rename(Flag_Secchi_m = Flag_Secchi)
 
 write.csv(secchi_reformat, '~/Reservoirs/Data/DataNotYetUploadedToEDI/Secchi/secchi_L1.csv')
+
+}
+
+secchi_qaqc()

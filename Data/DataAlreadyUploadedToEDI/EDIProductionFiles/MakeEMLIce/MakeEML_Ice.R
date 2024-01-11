cccb8b01b9f58332ed77d69899f8a795bc6c6846
cayelan <cayelan@vt.edu>
2024-01-10 20:05:25 -0500

    making final updates for ice metadata for production environment

--- Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEMLIce/MakeEML_Ice.R
+++ Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEMLIce/MakeEML_Ice.R
@@ -127,12 +127,7 @@ make_eml(path = "/Users/cayelan/Dropbox/ComputerFiles/Virginia_Tech/FallingCreek
          maintenance.description = "ongoing", 
          user.id = "ccarey",
          user.domain = 'EDI',
-         package.id = "edi.1076.3") #456.4 Put your package.id here, followed by .1 (for 1st version)
-
-#PROBLEMS WITH MAKING METATDATA! SO, COLIN SUGGESTED THAT THE FALLING CREEK SPACE IN THE PATH NAME WAS
-#  PROBLEMATIC, SO I COPIED AND PASTED THE ENTIRE DIRECTORY TO MY DESKTOP AND RAN THE MAKE_EML PATH THERE. THAT SEEMED TO WORK
-# ??!!! SO AM COPYING & PASTING THE .XML FILE BACK INTO THE GITHUB DIRECTORY. WORTH A TRY TO RUN IT OUT OF THERE
-# NEXT TIME WE UPDATE THE MET DATA IN THE FUTURE. I ALSO DELETED THE ZIP FILES 
+         package.id = "edi.456.5") #edi.1076.4 Put your package.id here, followed by .1 (for 1st version)
 
 
 # Once your xml file with your PUBLISHED package.id is Done, return to the 

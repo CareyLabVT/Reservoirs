dc93d5cf47274fc65b8e7fd6ee904e2f14ab6a48
Austin Delany <addelany@gmail.com>
2024-01-11 15:22:13 -0500

    Update ysi_create.R

--- Scripts/L1_functions/ysi_create.R
+++ Scripts/L1_functions/ysi_create.R
@@ -231,10 +231,10 @@ ysi$pH[!is.na(ysi$pH) & ysi$pH < 4] <- NA
 
 
 # ## identify latest date for data on EDI (need to add one (+1) to both dates because we want to exclude all possible start_day data and include all possible data for end_day)
-package_ID <- 'edi.198.11'
-eml <- read_metadata(package_ID)
-date_attribute <- xml_find_all(eml, xpath = ".//temporalCoverage/rangeOfDates/endDate/calendarDate")
-last_edi_date <- as.Date(xml_text(date_attribute)) + lubridate::days(1)
+#package_ID <- 'edi.198.11'
+#eml <- read_metadata(package_ID)
+#date_attribute <- xml_find_all(eml, xpath = ".//temporalCoverage/rangeOfDates/endDate/calendarDate")
+#last_edi_date <- as.Date(xml_text(date_attribute)) + lubridate::days(1)
 
 ysi <- ysi |> filter(DateTime > last_edi_date)
 

f19dba2576144954ca43bf206298d70245afd490
abbylewis <aslewis@vt.edu>
2024-01-19 13:20:46 -0500

    cut out unnecessary sourcing

--- Scripts/L1_generation_scripts/ctd_qaqc.R
+++ Scripts/L1_generation_scripts/ctd_qaqc.R
@@ -1,11 +1,6 @@
 pacman::p_load(oce, ocedata, tidyverse, lubridate)
 
 setwd("./Data/DataNotYetUploadedToEDI/Raw_CTD/CTD_code/CTD_automation/")
-source("ctd_functions_automated.R") #Load Carey Lab ctd functions
-source("flag_seasonal_csvs.R")
-source("process_CTD_file.R")
-source("identify_new_files.R")
-source("update_seasonal_csvs.R")
-source("ctd_qaqc.R")
+source("ctd_qaqc.R") #Load Carey Lab ctd functions
 
-ctd_QAQC(start_date = "2023-01-01") #run function using default values
+ctd_QAQC(start_date = "2024-01-01") #run function for this year using default values

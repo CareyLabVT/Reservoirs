0bce14017831186a1419335e40f3ed47e0654f93
abbylewis <aslewis@vt.edu>
2024-01-19 13:32:09 -0500

    fix file paths

--- Scripts/L1_generation_scripts/ctd_qaqc.R
+++ Scripts/L1_generation_scripts/ctd_qaqc.R
@@ -1,6 +1,6 @@
 pacman::p_load(oce, ocedata, tidyverse, lubridate)
 
-setwd("./Data/DataNotYetUploadedToEDI/Raw_CTD/CTD_code/CTD_automation/")
-source("ctd_qaqc.R") #Load Carey Lab ctd functions
+setwd("./Data/DataNotYetUploadedToEDI/Raw_CTD/CTD_code/")
+source("ctd_QAQC.R") #Load Carey Lab ctd functions
 
 ctd_QAQC(start_date = "2024-01-01") #run function for this year using default values

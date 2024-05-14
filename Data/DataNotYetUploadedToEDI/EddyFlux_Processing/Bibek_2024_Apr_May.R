b188e36cf7d77158690e837a26a182f7544eaa97
bee-bake <bibekkandel646@gmail.com>
2024-05-14 14:03:55 -0400

    script to use eddypro cleaning function created  only for bibek to use!

--- Data/DataNotYetUploadedToEDI/EddyFlux_Processing/Bibek_2024_Apr_May.R
+++
@@ -1,5 +0,0 @@
-source("C:/Users/13188/Desktop/Reservoirs/Data/DataNotYetUploadedToEDI/EddyFlux_Processing/eddy_pro_cleaning_function.R")
-Apr_May_2024 <- eddypro_cleaning_function(directory = "./Data/DataNotYetUploadedToEDI/EddyFlux_Processing/",
-                                  gdrive = T, # Are the files on Google Drive. True or False
-                                  gshared_drive = as_id("0ACybYKbCwLRPUk9PVA"),
-                                  current_year = 2024)
---
+++ Data/DataNotYetUploadedToEDI/EddyFlux_Processing/script_to_use_L1cleaning_function.R
@@ -0,0 +1,11 @@
+source("C:/Users/13188/Desktop/Reservoirs/Data/DataNotYetUploadedToEDI/EddyFlux_Processing/eddy_flux_create.R")
+
+Apr_May_2024 <- eddypro_cleaning_function(
+  directory = "./Data/DataNotYetUploadedToEDI/EddyFlux_Processing/",
+  gdrive = T, # Are the files on Google Drive. True or False
+  gshared_drive = as_id("0ACybYKbCwLRPUk9PVA"),
+  current_year = 2024,
+  output_file = "/EddyPro_Cleaned_L1.csv")
+  #start_date = as.Date("2023-12-31") + lubridate::days(1),
+  #end_date = Sys.Date() + lubridate::days(1))
+  

538d922f71f5a4593580a906651949df452dcad0
addelany <addelany@gmail.com>
2024-10-25 10:23:15 -0500

    add ice scripts

--- Scripts/L1_functions/daily_ice_cover_binary_create.R
+++ Scripts/L1_functions/daily_ice_cover_binary_create.R
@@ -167,7 +167,8 @@ daily_ice_cover_binary <- function(current_file, historic_wq_file, historic_file
     dplyr::full_join(combined_df, by = c('datetime')) |> 
     dplyr::mutate(Reservoir = ifelse(is.na(Reservoir), reservoir_name, Reservoir), 
                   #site_id = ifelse(is.na(site_id), site_identifier, site_id), 
-                  ice_presence = ifelse(is.na(ice_presence), 0, ice_presence), 
+                  #ice_presence = ifelse(is.na(ice_presence), 0, ice_presence), 
+                  ice_presence = ifelse(is.na(ice_presence), zoo::na.locf(ice_presence), ice_presence),
                   Method = ifelse(is.na(Method), 'T', Method), 
                   Flag = 0) |> 
     dplyr::rename(Date = datetime)
@@ -225,30 +226,30 @@ daily_ice_cover_binary <- function(current_file, historic_wq_file, historic_file
   return(daily_ice_df)
 }
 
-# current_files <- c("https://raw.githubusercontent.com/FLARE-forecast/BVRE-data/bvre-platform-data-qaqc/bvre-waterquality_L1.csv",
-#                    "https://raw.githubusercontent.com/FLARE-forecast/FCRE-data/fcre-catwalk-data-qaqc/fcre-waterquality_L1.csv")
-# 
-# historic_wq_files <- c('https://pasta.lternet.edu/package/data/eml/edi/725/4/9adadd2a7c2319e54227ab31a161ea12',
-#                        'https://pasta.lternet.edu/package/data/eml/edi/271/8/fbb8c7a0230f4587f1c6e11417fe9dce')
-# 
-# historic_ice_files <- c("https://pasta.lternet.edu/package/data/eml/edi/456/5/ebfaad16975326a7b874a21beb50c151")
-# 
-# ice_maintenance_log <- c('https://docs.google.com/spreadsheets/d/1viYhCGs3UgstzHEWdmP2Ig6uxyNM3ZC_uisG_R0QNpI/edit?gid=0#gid=0')
-# 
-# 
-# bvr_ice_data <- daily_ice_cover_binary(current_file = current_files[1], 
-#                                             historic_wq_file = historic_wq_files[1], 
-#                                             historic_file = historic_ice_files,
-#                                        maint_log = NULL, 
-#                                        ice_site = 'BVR')
-# 
-# fcr_ice_data <- daily_ice_cover_binary(current_file = current_files[2], 
-#                                             historic_wq_file = historic_wq_files[2], 
-#                                             historic_file = historic_ice_files,
-#                                        maint_log = ice_maintenance_log, 
-#                                        ice_site = 'FCR')
-# 
-# combined_ice_data <- dplyr::bind_rows(bvr_ice_data, fcr_ice_data)
+current_files <- c("https://raw.githubusercontent.com/FLARE-forecast/BVRE-data/bvre-platform-data-qaqc/bvre-waterquality_L1.csv",
+                   "https://raw.githubusercontent.com/FLARE-forecast/FCRE-data/fcre-catwalk-data-qaqc/fcre-waterquality_L1.csv")
+
+historic_wq_files <- c('https://pasta.lternet.edu/package/data/eml/edi/725/4/9adadd2a7c2319e54227ab31a161ea12',
+                       'https://pasta.lternet.edu/package/data/eml/edi/271/8/fbb8c7a0230f4587f1c6e11417fe9dce')
+
+historic_ice_files <- c("https://pasta.lternet.edu/package/data/eml/edi/456/5/ebfaad16975326a7b874a21beb50c151")
+
+ice_maintenance_log <- c('https://docs.google.com/spreadsheets/d/1viYhCGs3UgstzHEWdmP2Ig6uxyNM3ZC_uisG_R0QNpI/edit?gid=0#gid=0')
+
+
+bvr_ice_data <- daily_ice_cover_binary(current_file = current_files[1],
+                                            historic_wq_file = historic_wq_files[1],
+                                            historic_file = historic_ice_files,
+                                       maint_log = NULL,
+                                       ice_site = 'BVR')
+
+fcr_ice_data <- daily_ice_cover_binary(current_file = current_files[2],
+                                            historic_wq_file = historic_wq_files[2],
+                                            historic_file = historic_ice_files,
+                                       maint_log = ice_maintenance_log,
+                                       ice_site = 'FCR')
+
+combined_ice_data <- dplyr::bind_rows(bvr_ice_data, fcr_ice_data)
 
 #write.csv(combined_ice_data, "C:/Users/13188/Desktop/Data_repository/DataNotYetUploadedToEDI/Ice_binary/ice_L1.csv", row.names = FALSE)
 
--- Scripts/L1_functions/ice_transition_binary_create.R
+++ Scripts/L1_functions/ice_transition_binary_create.R
@@ -232,30 +232,30 @@ target_IceTransition_binary <- function(current_file, historic_wq_file, historic
   return(combined_df)
 }
 
-# current_files <- c("https://raw.githubusercontent.com/FLARE-forecast/BVRE-data/bvre-platform-data-qaqc/bvre-waterquality_L1.csv",
-#                    "https://raw.githubusercontent.com/FLARE-forecast/FCRE-data/fcre-catwalk-data-qaqc/fcre-waterquality_L1.csv")
-# 
-# historic_wq_files <- c('https://pasta.lternet.edu/package/data/eml/edi/725/4/9adadd2a7c2319e54227ab31a161ea12',
-#                        'https://pasta.lternet.edu/package/data/eml/edi/271/8/fbb8c7a0230f4587f1c6e11417fe9dce')
-# 
-# historic_ice_files <- c("https://pasta.lternet.edu/package/data/eml/edi/456/5/ebfaad16975326a7b874a21beb50c151")
-# 
-# ice_maintenance_log <- c('https://docs.google.com/spreadsheets/d/1viYhCGs3UgstzHEWdmP2Ig6uxyNM3ZC_uisG_R0QNpI/edit?gid=0#gid=0')
-# 
-# 
-# bvr_ice_data <- target_IceTransition_binary(current_file = current_files[1], 
-#                                             historic_wq_file = historic_wq_files[1], 
-#                                             historic_file = historic_ice_files, 
-#                                             ice_site = 'BVR',
-#                                             maint_log = NULL)
-# 
-# fcr_ice_data <- target_IceTransition_binary(current_file = current_files[2], 
-#                                             historic_wq_file = historic_wq_files[2], 
-#                                             historic_file = historic_ice_files,
-#                                             ice_site = "FCR",
-#                                             maint_log = ice_maintenance_log)
-# 
-# combined_ice_data <- dplyr::bind_rows(bvr_ice_data, fcr_ice_data) |> select(-site_id)
+current_files <- c("https://raw.githubusercontent.com/FLARE-forecast/BVRE-data/bvre-platform-data-qaqc/bvre-waterquality_L1.csv",
+                   "https://raw.githubusercontent.com/FLARE-forecast/FCRE-data/fcre-catwalk-data-qaqc/fcre-waterquality_L1.csv")
+
+historic_wq_files <- c('https://pasta.lternet.edu/package/data/eml/edi/725/4/9adadd2a7c2319e54227ab31a161ea12',
+                       'https://pasta.lternet.edu/package/data/eml/edi/271/8/fbb8c7a0230f4587f1c6e11417fe9dce')
+
+historic_ice_files <- c("https://pasta.lternet.edu/package/data/eml/edi/456/5/ebfaad16975326a7b874a21beb50c151")
+
+ice_maintenance_log <- c('https://docs.google.com/spreadsheets/d/1viYhCGs3UgstzHEWdmP2Ig6uxyNM3ZC_uisG_R0QNpI/edit?gid=0#gid=0')
+
+
+bvr_ice_data <- target_IceTransition_binary(current_file = current_files[1],
+                                            historic_wq_file = historic_wq_files[1],
+                                            historic_file = historic_ice_files,
+                                            ice_site = 'BVR',
+                                            maint_log = NULL)
+
+fcr_ice_data <- target_IceTransition_binary(current_file = current_files[2],
+                                            historic_wq_file = historic_wq_files[2],
+                                            historic_file = historic_ice_files,
+                                            ice_site = "FCR",
+                                            maint_log = ice_maintenance_log)
+
+combined_ice_data <- dplyr::bind_rows(bvr_ice_data, fcr_ice_data) |> select(-site_id)
 
 #write.csv(combined_ice_data, "C:/Users/13188/Desktop/Data_repository/DataNotYetUploadedToEDI/Ice_binary/ice_L1.csv", row.names = FALSE)
 

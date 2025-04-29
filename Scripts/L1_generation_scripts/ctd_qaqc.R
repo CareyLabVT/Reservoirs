1c627fd2e9de7dbcd2b9af78be4a1708d83eafdb
Adrienne Breef-Pilz <68744051+abreefpilz@users.noreply.github.com>
2025-04-28 22:35:25 -0400

    updated the edi package number

--- Scripts/L1_generation_scripts/ctd_qaqc.R
+++ Scripts/L1_generation_scripts/ctd_qaqc.R
@@ -6,7 +6,7 @@ source("ctd_QAQC.R") #Load Carey Lab ctd functions
 # Get the start date from the end of the CTD file on EDI
 
 ## identify latest date for data on EDI (need to add one (+1) to both dates because we want to exclude all possible start_day data and include all possible data for end_day)
-package_ID <- 'edi.200.14'
+package_ID <- 'edi.200.15'
 eml <- read_metadata(package_ID)
 date_attribute <- xml_find_all(eml, xpath = ".//temporalCoverage/rangeOfDates/endDate/calendarDate")
 last_edi_date <- as.Date(xml_text(date_attribute)) + lubridate::days(1)

23a410fb6052d3e6a56bc57b047ca22be4e94d4f
dwh77 <dwh1998@vt.edu>
2025-05-01 12:05:35 -0400

    restage bathymetry

--- Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEMLBathymetry/2025/MakeEMLBathymetry.R
+++ Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEMLBathymetry/2025/MakeEMLBathymetry.R
@@ -24,17 +24,17 @@ folder <- "./Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEMLBathymetry/
 make_eml(
   path = folder,
   dataset.title = "Bathymetry and watershed area for Falling Creek Reservoir, Beaverdam Reservoir, and Carvins Cove Reservoir",
-  temporal.coverage = c("2012-07-12", "2024-02-14"),
+  temporal.coverage = c("2012-07-12", "2024-04-14"),
   maintenance.description = 'completed',
   data.table = c("Bathymetry_comb.csv"),
   data.table.name = c("Bathymetric summary statistics"),
   data.table.description = c("Data table including bathymetric summary statistics for the three reservoirs"),
-  other.entity = c("Bathymetry_Spatial_Data.zip", "ADCP_SOP_11apr25.pdf"),
+  other.entity = c("Bathymetry_Spatial_Data.zip", "ADCP SOP.pdf"),
   other.entity.name = c("Bathymetry spatial data", "ADCP SOP"),
   other.entity.description = c("Spatial data for bathymetry from all reservoirs", "SOP for ADCP operation and post-processing of data and GIS file generation"),
   user.id = 'ccarey',
   user.domain = 'EDI',
-  package.id = 'edi.971.4') #971 is the staging number
+  package.id = 'edi.971.5') #971 is the staging number
 
 
 

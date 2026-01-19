d9aaf20c336a6dcdf076193745831e0120a7a18f
dwh77 <dwh1998@vt.edu>
2026-01-19 13:27:02 -0500

    update flagging for isotopes

--- Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEML_WaterIsotopes_CCR/2025/WaterIsotopes_QAQC.R
+++ Data/DataAlreadyUploadedToEDI/EDIProductionFiles/MakeEML_WaterIsotopes_CCR/2025/WaterIsotopes_QAQC.R
@@ -7,24 +7,25 @@ library(tidyverse)
 
 
 ##read in data
-iso <- read.csv("./Water_Isotopes/isotopes_named.csv")
+iso <- read.csv("https://raw.githubusercontent.com/dwh77/Reservoir_EEMs/refs/heads/main/Water_Isotopes/isotopes_named.csv")
 
 
 #### Make precipitation iso data frame
 iso_precip <- iso |> 
-  select(Reservoir, Site_number, Date, SampleName, d18O_VSMOW, d18O_VSMOW_1sd, d2H_VSMOW, d2H_VSMOW_1sd) |> 
   rename(Site = Site_number) |> 
-  filter(Site == 51)
+  filter(Site == 51) |> 
+  mutate(Iso_Flag = ifelse(Run_Name == "CCR3", 1, 0)) |> #Flag values for run date that was rerun
+  select(Reservoir, Site, Date, SampleName, d18O_VSMOW, d18O_VSMOW_1sd, d2H_VSMOW, d2H_VSMOW_1sd, Iso_Flag) 
 
 #write.csv(iso_precip, "./Water_Isotopes/EDI_2025/WaterIsotopes_precip_DRAFT.csv", row.names = F)
 
 #### Make reservoir iso data frame
 iso_reservoir <- iso |> 
-  select(Reservoir, Site_number, Date, Depth_m, d18O_VSMOW, d18O_VSMOW_1sd, d2H_VSMOW, d2H_VSMOW_1sd) |> 
   rename(Site = Site_number) |> 
   filter(Site != 51) |> 
-  mutate(Date = mdy(Date)) |> 
-  mutate(Iso_Flag = 0)
+  mutate(Iso_Flag = ifelse(Run_Name == "CCR3", 1, 0)) |> #Flag values for run date that was rerun
+  select(Reservoir, Site, Date, Depth_m, d18O_VSMOW, d18O_VSMOW_1sd, d2H_VSMOW, d2H_VSMOW_1sd, Iso_Flag) |> 
+  mutate(Date = mdy(Date)) 
 
-# write.csv(iso_reservoir, "./Water_Isotopes/EDI_2025/WaterIsotopes_CCR_2024_2025.csv", row.names = F)
+write.csv(iso_reservoir, "./Data/DataAlreadyUPloadedToEDI/EDIProductionFiles/MakeEML_WaterIsotopes_CCR/2025/WaterIsotopes_CCR_2024_2025.csv", row.names = F)
 
